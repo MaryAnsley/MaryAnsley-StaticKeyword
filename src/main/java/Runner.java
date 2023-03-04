6dd66949e33a1864d10f5239b57e5a6ba775d9ed
MaryAnsley <maryansley2021@yahoo.com>
2023-03-04 10:14:51 +0000

    Static Keyword

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -22,7 +22,6 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
-        Class.global_var="xyz";
-        System.out.println("xyz");
+        Class.global_var= in;
     }
 }
